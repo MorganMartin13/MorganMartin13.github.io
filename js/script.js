09a5002abacce2e9cad624ba79e8c99c5034f80e
MorganMartin13 <morgannnrosee13@gmail.com>
2025-05-07 10:07:19 -0400

    test

--- js/script.js
+++
@@ -1,7 +0,0 @@
-
-function handleSubmit() {
-    const name = document.getElementById('nameInput').value;
-    const message = name ? "Thank you, " + name + "!" : "Please enter your name.";
-    document.getElementById('response').innerHTML = message;
-    return false; // Prevent real form submission
-}
